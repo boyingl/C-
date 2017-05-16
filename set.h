0563dc31106704eb3686f094ce358783f0e2065c
Ashley Liu <liuboying@hotmail.com>
2017-05-15 20:53:18 -0400

    Create set.h

---
+++ set.h
@@ -0,0 +1,28 @@
+#ifndef _INTSET_H_
+#define _INTSET_H_
+#include <iostream>
+
+struct intSet {
+  int *data;
+  int size;
+  int capacity;
+  
+  intSet(); // constructor
+  intSet(const intSet& is); // copy constructor
+  ~intSet();  // destructor
+  intSet(intSet &&is); // move constructor
+  intSet &operator=(const intSet& is); // copy assignment operator
+  intSet &operator=(intSet &&is); // move assignment operator
+  void swap(intSet &is); // method to swap is fields with this object
+  intSet operator|(const intSet &other); // Set union
+  intSet operator&(const intSet &other); // Set intersection.
+  bool operator==(const intSet &other);  // Set equality.
+  bool isSubset(const intSet &s);  // True if s is a subset of *this
+  bool contains(int e) const;     // True if *this contains e
+  void add(int e);          // Add e to this set.
+  void remove(int e);       // Remove e from this set.
+};
+
+//output operator for intSet
+std::ostream& operator<<(std::ostream& out, const intSet& is);
+#endif
